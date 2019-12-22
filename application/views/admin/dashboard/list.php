8dd5f7e694582e143a2619fa183e15f3c448e468
iam112 <dimasadityamukhsinin@gmail.com>
2019-12-22 19:45:18 +0700

    a

--- application/views/admin/dashboard/list.php
+++ application/views/admin/dashboard/list.php
@@ -1086,63 +1086,6 @@
           });
     </script>
 
-    <!-- INHIL -->
-    <script>
-        //-------------
-        //- PIE CHART-
-        //-------------
-        // Get context with jQuery - using jQuery's .get() method.
-        var pieChartCanvas = $("#pieChart-inhil").get(0).getContext("2d");
-        var pieChart = new Chart(pieChartCanvas);
-        var PieData = [
-          {
-            value: <?php echo $belum_inhil ?>,
-            color: "#f56954",
-            highlight: "#f56954",
-            label: "Belum Diproses"
-          },
-          {
-            value: <?php echo $sudah_inhil ?>,
-            color: "#FF7F00",
-            highlight: "#FF7F00",
-            label: "Sudah Diproses"
-          },
-          {
-            value: <?php echo $selesai_inhil ?>,
-            color: "#00a65a",
-            highlight: "#00a65a",
-            label: "Selesai"
-          },
-        ];
-        var pieOptions = {
-          //Boolean - Whether we should show a stroke on each segment
-          segmentShowStroke: true,
-          //String - The colour of each segment stroke
-          segmentStrokeColor: "#fff",
-          //Number - The width of each segment stroke
-          segmentStrokeWidth: 2,
-          //Number - The percentage of the chart that we cut out of the middle
-          percentageInnerCutout: 50, // This is 0 for Pie charts
-          //Number - Amount of animation steps
-          animationSteps: 100,
-          //String - Animation easing effect
-          animationEasing: "easeOutBounce",
-          //Boolean - Whether we animate the rotation of the Doughnut
-          animateRotate: true,
-          //Boolean - Whether we animate scaling the Doughnut from the centre
-          animateScale: false,
-          //Boolean - whether to make the chart responsive to window resizing
-          responsive: true,
-          // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
-          maintainAspectRatio: true,
-          //String - A legend template
-          legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
-        };
-        //Create pie or douhnut chart
-        // You can switch between pie and douhnut using the method below.
-        pieChart.Doughnut(PieData, pieOptions);
-    </script>
-
     <!-- BENGKALIS -->
     <script>
         //-------------
