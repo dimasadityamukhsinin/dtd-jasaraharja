399fa04a3de46f588fc659ee334d86c45aaae8f3
iam112 <dimasadityamukhsinin@gmail.com>
2019-12-24 16:05:00 +0700

    a

---
+++ application/views/admin/grafik_pencapaian/cetak.php
@@ -0,0 +1,5 @@
+<div class="btn-group ">
+    <a href="" title="Cetak" data-toggle="modal" data-target="#cetak" class="btn btn-success btn-lg">
+        <i class="fa fa-print"></i> Cetak
+    </a>
+</div>
